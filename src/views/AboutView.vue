baa043d5aabbf28f37608d6fbce461ea53a0d4d8
KitM17 <maria-v-52rus@yandex.ru>
2022-06-13 21:18:33 +0300

    About  кнопки с событием лайк и дизлайк (счетчик)

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div>
+      <button @click="addLike">Like</button>
+      <button @click="addDislike">Dislike</button>
+    </div>
+    <div class="like">Кол-во лайков: {{ likes }}</div>
+    <div class="dislike">Кол-во дизлайков: {{ dislikes }}</div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    };
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    },
+  },
+};
+</script>
 
 <style>
 @media (min-width: 1024px) {
@@ -11,5 +34,11 @@
     display: flex;
     align-items: center;
   }
+}
+.like {
+  color: green;
+}
+.dislike {
+  color: rebeccapurple;
 }
 </style>
