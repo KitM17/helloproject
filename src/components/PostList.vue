c9b7d9c62fc891d16a85644f95008615cd5cc03d
KitM17 <maria-v-52rus@yandex.ru>
2022-06-20 11:18:30 +0300

    декомпоз посты и обмен м-ду родителем и дочерним

---
+++ src/components/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="post" v-for="post in posts">
+    <div>Название: {{ post.title }}</div>
+    <div>Описание: {{ post.body }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.post {
+  padding: 2rem;
+  border: 2px solid teal;
+  margin-top: 2rem;
+}
+</style>
--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -1,32 +1,19 @@
 <template>
   <div class="app">
-    <form @submit.prevent>
-      <h4>Создание поста</h4>
-      <input
-        v-bind:value="title"
-        @input="title=$event.target.value"
-        class="input"
-        type="text"
-        placeholder="Название"
-      />
-      <input
-        v-bind:value="body"
-          @input="body=$event.target.value"
-        class="input"
-        type="text"
-        placeholder="Описание"
-      />
-      <button class="btn" @click="createPost">Создать</button>
-    </form>
-    <div class="post" v-for="post in posts">
-      <div>Название: {{ post.title }}</div>
-      <div>Описание: {{ post.body }}</div>
-    </div>
+    <post-form @create="createPost" />
+    <post-list :posts="posts" />
   </div>
 </template>
 
 <script>
+import PostForm from "@/components/PostForm.vue";
+import PostList from "@/components/PostList.vue";
+
 export default {
+  components: {
+    PostList,
+    PostForm,
+  },
   data() {
     return {
       posts: [
@@ -35,51 +22,18 @@ export default {
         { id: 3, title: "JavaScript 3", body: "Описание поста 3" },
         { id: 4, title: "JavaScript 4", body: "Описание поста 4" },
       ],
-      title: "",
-      body: "",
     };
   },
   methods: {
-    createPost() {
-      const newPost={
-        id: Date.now(),
-        title:this.title,
-        body: this.body,
-      }
-      this.posts.push(newPost);
-      this.title="";
-      this.body="";
+    createPost(post) {
+      this.posts.push(post);
     },
- },
-  
+  },
 };
 </script>
 
 <style>
 .app {
   pad: 2.5rem;
-}
-.post {
-  padding: 2rem;
-  border: 2px solid teal;
-  margin-top: 2rem;
-}
-form {
-  display: flex;
-  flex-direction: column;
-}
-.input {
-  width: 100%;
-  border: 1px solid teal;
-  padding: 10px 15px;
-  margin-top: 1rem;
-}
-.btn {
-  margin-top: 1rem;
-  align-self: flex-end;
-  padding: 10px 15px;
-  background: none;
-  color: teal;
-  border: 1px solid teal;
 }
 </style>
